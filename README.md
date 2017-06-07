8e6fc04ba7dcd36bfb649356c509a65c4c913f9d
Colin Valliant <alcarithemad@gmail.com>
2017-06-07 00:37:52 -0700

    First real commit.

--- README.md
+++ README.md
@@ -1,2 +1,69 @@
 # typetrace
-Trace Python execution and record type information.
+
+Trace Python execution and record type information for function calls.
+Produces type annotations which strongly resemble [PEP 484](https://www.python.org/dev/peps/pep-0484/) "type comments".
+Probably works in Python 2 and 3.
+
+
+# Usage
+
+~~~python
+import typetrace
+tt = typetrace.TypeTracer()
+
+...
+
+# start tracing
+tt.set_trace()
+
+# do some interesting stuff here
+# consider invoking a test suite, or large sample application
+
+# end tracing
+tt.end_trace()
+
+# print traced type annotations
+tt.print_types()
+
+# or write them to a file by passing a file object to tt.write_types(f)
+~~~
+
+Output looks like this:
+
+~~~
+lib/python2.7/inspect.py:155 isgeneratorfunction   # type: (function) -> bool
+lib/python2.7/UserDict.py:103 __contains__     # type: (str) -> bool
+lib/python2.7/UserDict.py:91 get   # type: (str, Union[None, int]) -> Union[None, int]
+lib/python2.7/encodings/__init__.py:49 normalize_encoding      # type: (str) -> str
+lib/python2.7/encodings/__init__.py:71 search_function # type: (str) -> Tuple[builtin_function_or_method, function, classobj, classobj]
+lib/python2.7/encodings/utf_8.py:15 decode     # type: (Union[buffer, str], str) -> Tuple[unicode, int]
+lib/python2.7/encodings/utf_8.py:33 getregentry        # type: () -> Tuple[builtin_function_or_method, function, classobj, classobj]
+lib/python2.7/posixpath.py:120 dirname # type: (str) -> str
+lib/python2.7/posixpath.py:329 normpath        # type: (str) -> str
+lib/python2.7/posixpath.py:358 abspath # type: (str) -> str
+lib/python2.7/posixpath.py:52 isabs    # type: (str) -> bool
+lib/python2.7/posixpath.py:82 split    # type: (str) -> Tuple[str, str]
+lib/python2.7/re.py:138 match  # type: (str, str, int) -> Union[SRE_Match, None]
+lib/python2.7/re.py:148 sub    # type: (str, str, str, int, int) -> str
+lib/python2.7/re.py:192 compile        # type: (str, int) -> SRE_Pattern
+lib/python2.7/re.py:230 _compile       # type: (*Union[str, int]) -> SRE_Pattern
+~~~
+
+# Caveats
+
+It works, but:
+
+* It's slow.
+* It may generate some nonsensical type annotations.
+* It only outputs the Python 2 compatible type comment syntax.
+* If you want to add the generated annotations to the source automatically, you'll need to write more code.
+* Did I mention it's slow?
+* It doesn't handle functions returning multiple lengths of tuples very well. It produces a union of each length it's seen.
+* Doesn't simplify `Union[T, None]` to `Optional[T]`.
+* It's poorly tested.
+* Things that raise exceptions will include `None` among their return types, even if None is never otherwise returned.
+* Probably doesn't handle `@classmethod` correctly.
+
+# License
+
+MIT license.
---
+++ typetrace.py
@@ -0,0 +1,152 @@
+import inspect
+import sys
+
+import collections
+
+
+def function_id(frame):
+    code = frame.f_code
+    ident = code.co_filename, code.co_firstlineno, code.co_name
+    return ident
+
+
+def type_from(arg):
+    if isinstance(arg, list) and len(arg) > 0:
+        return type(arg), frozenset(type(a) for a in arg)
+    if isinstance(arg, tuple) and len(arg) > 0:
+        return type(arg), tuple(type(a) for a in arg)
+    elif isinstance(arg, dict) and len(arg) > 0:
+        return dict, frozenset(type(a) for a in arg.keys()), frozenset(type(a) for a in arg.values())
+    else:
+        return type(arg)
+
+
+class Function(object):
+    def __init__(self):
+        self.args = collections.OrderedDict()
+        self.varargs = set()
+        self.kwargs = set()
+        self.returns = set()
+
+    def add_call(self, frame):
+        arginfo = inspect.getargvalues(frame)
+        if len(arginfo.args) > 0 and arginfo.args[0] == 'self':
+            arginfo.args.pop(0)
+        for arg in arginfo.args:
+            if arg not in self.args:
+                self.args[arg] = set()
+            self.args[arg].add(type_from(arginfo.locals[arg]))
+        self.varargs |= set(type_from(x) for x in arginfo.locals.get(arginfo.varargs, ()))
+        self.kwargs |= set(type_from(x) for x in arginfo.locals.get(arginfo.keywords, {}).values())
+
+    def add_return(self, arg):
+        self.returns.add(type_from(arg))
+
+
+class TypeTracer(object):
+    def __init__(self):
+        self.funcs = collections.defaultdict(Function)
+
+        code = TypeTracer.end_trace.__code__
+        self.end_ident = code.co_filename, code.co_firstlineno, code.co_name
+
+    def __call__(self, frame, event, arg):
+        ident = function_id(frame)
+        # don't trace the function that ends tracing, we'll never see its return type
+        if ident == self.end_ident:
+            return self
+        if ident[2].startswith('<'):
+            return self
+        if event == 'call':
+            self.funcs[ident].add_call(frame)
+            return self
+        elif event == 'return':
+            self.funcs[ident].add_return(arg)
+
+    def set_trace(self):
+        self.old = sys.gettrace()
+        sys.settrace(self)
+
+    def end_trace(self):
+        sys.settrace(self.old)
+
+    def stringify_types(self):
+        for func, data in self.funcs.items():
+            yield '{}:{}\t{}'.format(*func) + '\t# type: ' + format_args(data)
+
+    def print_types(self):
+        for l in self.stringify_types():
+            print(l)
+
+    def write_types(self, fileobj):
+        for l in self.stringify_types():
+            fileobj.write(l+'\n')
+
+
+def format_types(t):
+    if isinstance(t, (set, frozenset)) and len(t) > 1:
+        return 'Union[' + ', '.join(format_types(x) for x in t) + ']'
+    elif isinstance(t, (set, frozenset)) and len(t) == 1:
+        return format_types(list(t)[0])
+    elif isinstance(t, tuple):
+        if issubclass(t[0], list):
+            return 'List[' + format_types(t[1]) + ']'
+        elif issubclass(t[0], tuple):
+            return 'Tuple[' + ', '.join(format_types(x) for x in t[1]) + ']'
+        elif issubclass(t[0], dict):
+            return 'Dict[' + format_types(t[1]) + ', ' + format_types(t[2]) + ']'
+    elif t == type(None):
+        return 'None'
+    else:
+        return t.__name__
+
+
+def format_args(data):
+    args = data.args
+    varargs = data.varargs
+    kwargs = data.kwargs
+    returns = data.returns
+    out = []
+    for arg, types in args.items():
+        out.append(format_types(types))
+    if varargs:
+        out.append('*' + format_types(varargs))
+    if kwargs:
+        out.append('**' + format_types(kwargs))
+    arg_str = '(' + ', '.join(out) + ')'
+    arg_str += ' -> ' + format_types(returns)
+    return arg_str
+
+if __name__ == '__main__':
+    def foo(x, y):
+        # type:  (Union[float, int], int) -> Union[float, int]
+        return x + y
+
+    def bar(x, y):
+        return x + y
+
+    def baz(x, y=3):
+        return x + y
+
+    def kwa(*a, **kw):
+        # type:  (*Union[str, int], **Union[tuple, dict]) -> NoneType
+        return None
+
+    tt = TypeTracer()
+
+    tt.set_trace()
+
+
+    z = 5
+    foo(1, 3)
+    foo(1.0, 3)
+
+    bar('a', 'b')
+    baz(1)
+    baz(x=2)
+    kwa()
+    kwa(1, '', a=(1, ''), b={'': None})
+
+    tt.end_trace()
+
+    tt.print_types()
